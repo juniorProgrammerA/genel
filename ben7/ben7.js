c7d566a5324161b2d8c2ce04c189d813d40ccf3d
Ahmet <ahmetfarukeren272@gmail.com>
2024-04-26 03:03:39 +0300

    wep site

---
+++ ben7/ben7.js
@@ -0,0 +1,141 @@
+
+
+
+document.querySelector(".button1").addEventListener("click", () => {
+    document.querySelector(".önemli").style.marginLeft = "350px";
+    document.querySelector(".önemli").style.borderRadius = "0px 30px 30px 0px";
+    document.querySelector(".button1").style.marginLeft = "80px";
+    document.querySelector(".button1").style.transition = "1s";
+    document.querySelector(".button1").textContent = "Sign In";
+    document.querySelector(".h1").textContent = "Sign Up";
+    document.querySelector(".button").textContent = "Sign Up";
+    document.querySelector(".button1").addEventListener("click", () => {
+        document.querySelector(".önemli").style.marginLeft = "0px";
+        document.querySelector(".önemli").style.borderRadius = "30px -00px 00px 30px";
+        document.querySelector(".button1").style.marginLeft = "451px";
+        document.querySelector(".button1").textContent = "Sign Up";
+        document.querySelector(".h1").textContent = "Sign In";
+        document.querySelector(".button").textContent = "Sign In";
+        document.querySelector(".button1").addEventListener("click", () => {
+            document.querySelector(".önemli").style.marginLeft = "350px";
+            document.querySelector(".önemli").style.borderRadius = "0px 30px 30px 0px";
+            document.querySelector(".button1").style.marginLeft = "80px";
+            document.querySelector(".button1").style.transition = "1s";
+            document.querySelector(".button1").textContent = "Sign In";
+            document.querySelector(".h1").textContent = "Sign Up";
+            document.querySelector(".button").textContent = "Sign Up";
+            document.querySelector(".button1").addEventListener("click", () => {
+                document.querySelector(".önemli").style.marginLeft = "0px";
+                document.querySelector(".önemli").style.borderRadius = "30px -00px 00px 30px";
+                document.querySelector(".button1").style.marginLeft = "451px";
+                document.querySelector(".button1").textContent = "Sign Up";
+                document.querySelector(".h1").textContent = "Sign In";
+                document.querySelector(".button").textContent = "Sign In";
+                document.querySelector(".button1").addEventListener("click", () => {
+                    document.querySelector(".önemli").style.marginLeft = "350px";
+                    document.querySelector(".önemli").style.borderRadius = "0px 30px 30px 0px";
+                    document.querySelector(".button1").style.marginLeft = "80px";
+                    document.querySelector(".button1").style.transition = "1s";
+                    document.querySelector(".button1").textContent = "Sign In";
+                    document.querySelector(".h1").textContent = "Sign Up";
+                    document.querySelector(".button").textContent = "Sign Up";
+                    document.querySelector(".button1").addEventListener("click", () => {
+                        document.querySelector(".önemli").style.marginLeft = "0px";
+                        document.querySelector(".önemli").style.borderRadius = "30px -00px 00px 30px";
+                        document.querySelector(".button1").style.marginLeft = "451px";
+                        document.querySelector(".button1").textContent = "Sign Up";
+                        document.querySelector(".h1").textContent = "Sign In";
+                        document.querySelector(".button").textContent = "Sign In";
+                        document.querySelector(".button1").addEventListener("click", () => {
+                            document.querySelector(".önemli").style.marginLeft = "350px";
+                            document.querySelector(".önemli").style.borderRadius = "0px 30px 30px 0px";
+                            document.querySelector(".button1").style.marginLeft = "80px";
+                            document.querySelector(".button1").style.transition = "1s";
+                            document.querySelector(".button1").textContent = "Sign In";
+                            document.querySelector(".h1").textContent = "Sign Up";
+                            document.querySelector(".button").textContent = "Sign Up";
+                            document.querySelector(".button1").addEventListener("click", () => {
+                                document.querySelector(".önemli").style.marginLeft = "0px";
+                                document.querySelector(".önemli").style.borderRadius = "30px -00px 00px 30px";
+                                document.querySelector(".button1").style.marginLeft = "451px";
+                                document.querySelector(".button1").textContent = "Sign Up";
+                                document.querySelector(".h1").textContent = "Sign In";
+                                document.querySelector(".button").textContent = "Sign In";
+                                document.querySelector(".button1").addEventListener("click", () => {
+                                    document.querySelector(".önemli").style.marginLeft = "350px";
+                                    document.querySelector(".önemli").style.borderRadius = "0px 30px 30px 0px";
+                                    document.querySelector(".button1").style.marginLeft = "80px";
+                                    document.querySelector(".button1").style.transition = "1s";
+                                    document.querySelector(".button1").textContent = "Sign In";
+                                    document.querySelector(".h1").textContent = "Sign Up";
+                                    document.querySelector(".button").textContent = "Sign Up";
+                                    document.querySelector(".button1").addEventListener("click", () => {
+                                        document.querySelector(".önemli").style.marginLeft = "0px";
+                                        document.querySelector(".önemli").style.borderRadius = "30px -00px 00px 30px";
+                                        document.querySelector(".button1").style.marginLeft = "451px";
+                                        document.querySelector(".button1").textContent = "Sign Up";
+                                        document.querySelector(".h1").textContent = "Sign In";
+                                        document.querySelector(".button").textContent = "Sign In";
+                                        document.querySelector(".button1").addEventListener("click", () => {
+                                            document.querySelector(".önemli").style.marginLeft = "350px";
+                                            document.querySelector(".önemli").style.borderRadius = "0px 30px 30px 0px";
+                                            document.querySelector(".button1").style.marginLeft = "80px";
+                                            document.querySelector(".button1").style.transition = "1s";
+                                            document.querySelector(".button1").textContent = "Sign In";
+                                            document.querySelector(".h1").textContent = "Sign Up";
+                                            document.querySelector(".button").textContent = "Sign Up";
+                                            document.querySelector(".button1").addEventListener("click", () => {
+                                                document.querySelector(".önemli").style.marginLeft = "0px";
+                                                document.querySelector(".önemli").style.borderRadius = "30px -00px 00px 30px";
+                                                document.querySelector(".button1").style.marginLeft = "451px";
+                                                document.querySelector(".button1").textContent = "Sign Up";
+                                                document.querySelector(".h1").textContent = "Sign In";
+                                                document.querySelector(".button").textContent = "Sign In";
+                                                document.querySelector(".button1").addEventListener("click", () => {
+                                                    document.querySelector(".önemli").style.marginLeft = "350px";
+                                                    document.querySelector(".önemli").style.borderRadius = "0px 30px 30px 0px";
+                                                    document.querySelector(".button1").style.marginLeft = "80px";
+                                                    document.querySelector(".button1").style.transition = "1s";
+                                                    document.querySelector(".button1").textContent = "Sign In";
+                                                    document.querySelector(".h1").textContent = "Sign Up";
+                                                    document.querySelector(".button").textContent = "Sign Up";
+                                                    document.querySelector(".button1").addEventListener("click", () => {
+                                                        document.querySelector(".önemli").style.marginLeft = "0px";
+                                                        document.querySelector(".önemli").style.borderRadius = "30px -00px 00px 30px";
+                                                        document.querySelector(".button1").style.marginLeft = "451px";
+                                                        document.querySelector(".button1").textContent = "Sign Up";
+                                                        document.querySelector(".h1").textContent = "Sign In";
+                                                        document.querySelector(".button").textContent = "Sign In";
+                                                        document.querySelector(".button1").addEventListener("click", () => {
+                                                            document.querySelector(".önemli").style.marginLeft = "350px";
+                                                            document.querySelector(".önemli").style.borderRadius = "0px 30px 30px 0px";
+                                                            document.querySelector(".button1").style.marginLeft = "80px";
+                                                            document.querySelector(".button1").style.transition = "1s";
+                                                            document.querySelector(".button1").textContent = "Sign In";
+                                                            document.querySelector(".h1").textContent = "Sign Up";
+                                                            document.querySelector(".button").textContent = "Sign Up";
+                                                            document.querySelector(".button1").addEventListener("click", () => {
+                                                                document.querySelector(".önemli").style.marginLeft = "0px";
+                                                                document.querySelector(".önemli").style.borderRadius = "30px -00px 00px 30px";
+                                                                document.querySelector(".button1").style.marginLeft = "451px";
+                                                                document.querySelector(".button1").textContent = "Sign Up";
+                                                                document.querySelector(".h1").textContent = "Sign In";
+                                                                document.querySelector(".button").textContent = "Sign In";
+                                                            })
+                                                        })
+                                                    })
+                                                })
+                                            })
+                                        })
+                                    })
+                                })
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    })
+})
+
+
